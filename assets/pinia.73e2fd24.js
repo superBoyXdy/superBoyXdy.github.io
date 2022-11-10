5e045477172bcb2a1fcf1441c5a54b22049abcab
xiaodeyu <xiaodeyu@meituan.com>
2022-11-11 07:11:37 +0800

    feat: 94%

---
+++ assets/pinia.73e2fd24.js
@@ -0,0 +1,1 @@
+import{m as r}from"./index.777c7bcd.js";const s=r("Public",{state:()=>({userMsg:"token",selectVideo:{imagekey:"",imagevideo:""},searchkey:""}),getters:{getUserMsg:e=>e.userMsg}});export{s as P};
