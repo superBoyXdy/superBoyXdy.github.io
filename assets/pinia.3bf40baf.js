3271cac21aa9521f1053a62458306cfb2fe48f4f
xiaodeyu <xiaodeyu@meituan.com>
2022-11-11 06:44:30 +0800

    feat: 93%

--- assets/pinia.3bf40baf.js
+++
@@ -1,1 +0,0 @@
-import{m as r}from"./index.14665f73.js";const s=r("Public",{state:()=>({userMsg:"token",selectVideo:{imagekey:"",imagevideo:""},searchkey:""}),getters:{getUserMsg:e=>e.userMsg}});export{s as P};
