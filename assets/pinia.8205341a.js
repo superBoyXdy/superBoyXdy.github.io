07ebff48a2f9129bb2f8f38a3708dcfe9cdbd6c2
xiaodeyu <xiaodeyu@meituan.com>
2022-11-10 21:01:35 +0800

    feat: update

--- assets/pinia.8205341a.js
+++
@@ -1,1 +0,0 @@
-import{l as r}from"./index.f58769d7.js";const s=r("Public",{state:()=>({userMsg:"token"}),getters:{getUserMsg:e=>e.userMsg}});export{s as P};
---
+++ assets/pinia.9ea4a4a4.js
@@ -0,0 +1,1 @@
+import{l as r}from"./index.84d40691.js";const s=r("Public",{state:()=>({userMsg:"token"}),getters:{getUserMsg:e=>e.userMsg}});export{s as P};
