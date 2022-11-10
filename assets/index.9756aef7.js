e41c2583b9dc0c2a16786df9b2a0738cbbff2097
xiaodeyu <xiaodeyu@meituan.com>
2022-11-10 20:44:57 +0800

    feat: init

---
+++ assets/index.9756aef7.js
@@ -0,0 +1,1 @@
+import{_ as o,r,o as c,c as n}from"./index.6d6c6eff.js";const t={};function _(s,a){const e=r("router-view");return c(),n(e)}var i=o(t,[["render",_]]);export{i as default};
---
+++ assets/pinia.5a02b079.js
@@ -0,0 +1,1 @@
+import{l as r}from"./index.6d6c6eff.js";const s=r("Public",{state:()=>({userMsg:"token"}),getters:{getUserMsg:e=>e.userMsg}});export{s as P};
